c977954a6be928f9fa3f6d32573d44b77a05cfa7
Joon Ho Cho <joonho1101@gmail.com>
2016-05-02 17:45:03 -0700

    Level based permission

---
+++ src/index.js
@@ -0,0 +1,58 @@
+export default class Permissions() {
+  constructor({
+    admins,
+    defaultLevel = 0,
+    authenticatedLevel = 1,
+    adminLevel = 10,
+  }) {
+    adminMap = (admins || []).reduce((obj, id) => {
+      obj[id] = true;
+    }, {});
+
+    this.check = ({p = defaultLevel, a, b}, userId, userLevel) => {
+      const isAuthed = userId != null;
+
+      if (userLevel == null) {
+        if (adminMap[userId] === true) {
+          userLevel = adminLevel;
+        } else if (isAuthed) {
+          userLevel = authenticatedLevel;
+        } else {
+          userLevel = 0;
+        }
+      }
+
+      if (userLevel >= p) {
+        return !(isAuthed && b && b.indexOf(userId) >= 0);
+      }
+
+      return Boolean(isAuthed && a && a.indexOf(userId) >= 0);
+    };
+  }
+}
+/*
+Permission Object
+
+{
+  c: {
+    p: 2,
+    a: [],
+    b: [],
+  },
+  r: {
+    p: 0,
+    a: [],
+    b: [],
+  },
+  u: {
+    p: 2,
+    a: [],
+    b: [],
+  },
+  d: {
+    p: 3,
+    a: [],
+    b: [],
+  },
+}
+*/
